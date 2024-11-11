a748dc278909cd76329cc43906626538aac7130b
shashankch2003 <122424766+shashankch2003@users.noreply.github.com>
2024-11-11 13:56:46 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,4 +1,4 @@
-const getTotalIsles = function (grid)  if (!grid || grid.length === 0) return 0;
+const getTotalIsles = function (grid){  if (!grid || grid.length === 0) return 0;
 
 let numIslands = 0;
 
